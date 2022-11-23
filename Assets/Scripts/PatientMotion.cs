4c8039ea31e58c4d1a0ab08e17793b5e1e8d6fd9
hyeok218 <102659958+hyeok218@users.noreply.github.com>
2022-11-23 21:29:48 +0900

    첫 씬 구현
    환자 범위 안에 들어오면 환자 쓰러지는 에니메이션,
    환자와 거리 가까워졌을때 'F'키를 눌러서 상호작용 하면 씬2 이동
    +(플레이어 이동,카메라 이동 스크립트)

---
+++ Assets/Scripts/PatientMotion.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PatientMotion : MonoBehaviour
+{
+    public int count = 0;
+    public int cnt = 1;
+    private Animator m_animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_animator = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 15.0f))
+        {
+            Debug.Log("범위안으로 들어옴");
+            /*if (Input.GetKeyDown(KeyCode.w))
+            {
+                */
+            if (cnt == 1)
+                {
+                    count = 1;
+                    m_animator.SetTrigger("faint");
+                    m_animator.SetFloat("count", count);
+                    cnt = 0;
+                    
+                }
+           // }
+
+        }
+    }
+}
