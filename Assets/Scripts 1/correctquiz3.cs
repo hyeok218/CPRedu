89c0ed91f59ac29e087b9b6c59852ec4ebc25053
hyeok218 <102659958+hyeok218@users.noreply.github.com>
2022-12-18 03:04:55 +0900

    최종
    퀴즈 추가

---
+++ Assets/Scripts 1/correctquiz3.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+[RequireComponent(typeof(Collider))]
+public class correctquiz3 : MonoBehaviour
+{
+    public Image LoadingBar;
+    private bool IsOn;
+    private float barTime = 0.0f;
+    public int count;
+    public GameObject player;
+
+
+    void Start()
+    {
+        IsOn = false;
+        LoadingBar.fillAmount = 0;
+    }
+
+
+    void Update()
+    {
+        if (IsOn)
+        {
+            Debug.Log(barTime);
+            if (barTime <= 1.0f)
+            {
+                barTime += Time.deltaTime;
+            }
+            LoadingBar.fillAmount = barTime / 1.0f;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+
+    }
+    public void SetGazedAt(bool gazedAt)
+    {
+        IsOn = gazedAt;
+        barTime = 0.0f;
+        if (gazedAt)
+        {
+            Invoke("goNext", 1.0f);
+        }
+        else
+        {
+            Debug.Log("Out");
+            LoadingBar.fillAmount = 0;
+        }
+    }
+
+    void goNext()
+    {
+        if (barTime >= 0.8f)
+        {
+            player.transform.position = new Vector3(5270, 4578, -4809);
+        }
+    }
+
+}
---
+++ Assets/Scripts 1/wrongquiz.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+[RequireComponent(typeof(Collider))]
+public class wrongquiz : MonoBehaviour
+{
+    public GameObject talkPanel;
+    public Image LoadingBar;
+    private bool IsOn;
+    private float barTime = 0.0f;
+    public int count;
+
+
+    void Start()
+    {
+        IsOn = false;
+        LoadingBar.fillAmount = 0;
+        talkPanel.SetActive(false);
+    }
+
+
+    void Update()
+    {
+        if (IsOn)
+        {
+
+            if (barTime <= 1.0f)
+            {
+                barTime += Time.deltaTime;
+            }
+            LoadingBar.fillAmount = barTime / 1.0f;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+
+    }
+    public void SetGazedAt(bool gazedAt)
+    {
+        IsOn = gazedAt;
+        barTime = 0.0f;
+        if (gazedAt)
+        {
+            Invoke("showText", 1.0f);
+        }
+        else
+        {
+            Debug.Log("Out");
+            LoadingBar.fillAmount = 0;
+        }
+    }
+
+    void showText()
+    {
+        if (barTime >= 0.8f)
+        {
+            Debug.Log("hi");
+            talkPanel.SetActive(true);
+            Invoke("exitText", 2.0f);
+        }
+    }
+
+    void exitText()
+    {
+        Debug.Log("bye");
+        talkPanel.SetActive(false);
+        //GameObject.Find("TT_demo_male_A").GetComponent<Scene1Num>().num++;
+    }
+}
